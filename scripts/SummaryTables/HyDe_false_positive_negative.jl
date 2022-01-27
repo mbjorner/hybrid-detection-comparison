727957e1b67ec3da66911b759b6d2cc3b4924efa
Marianne Bjørner <mbbjorner@gmail.com>
2022-01-26 23:37:01 -0600

    add naming conventions HyDe summary table

--- scripts/SummaryTables/HyDe_false_positive_negative.jl
+++ scripts/SummaryTables/HyDe_false_positive_negative.jl
@@ -9,14 +9,19 @@ trees="n15"
 total_false = DataFrame(gene_trees = Float64[],  trial_num = Float64[], seq_length = Float64[], 
                         HyDe_fp = Float64[], HyDe_fn = Float64[], HyDe_tp = Float64[], HyDe_tn = Float64[], HyDe_wrongClade = Float64[])
 
+network_names = ["n10","n10orange","n10red","n15","n15blue","n15orange","n15red"]
+
 num_gene_trees = [30,100,300,1000,3000]
-seq_lengths = [10000,50000,100000]
+seq_lengths = [10000,30000,50000,100000,250000,500000]
 # num_gene_trees = [50, 100, 500, 1000]
-for seq_length in seq_lengths
+for net_names in network_names
+    net_num = net_names[1:3]
+    for seq_length in seq_lengths
     for num_trees in num_gene_trees
         for file_number in 1:30
-            file = DataFrame(CSV.File(string("seq", seq_length, "/", trees, "_n", num_trees, "/", trees,"_", num_trees, "_", file_number, "_", seq_length, "_HyDe.csv")))
+            filename = string(net_names,"/HyDe_Dstat/",net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat.csv")
 
+            file = DataFrame(CSV.File(filename))
             insertcols!(file, size(file, 2) + 1, :FP => 0);
             insertcols!(file, size(file, 2) + 1, :FN => 0);
             insertcols!(file, size(file, 2) + 1, :TP => 0);
@@ -63,6 +68,7 @@ for seq_length in seq_lengths
         end 
     end
 end
+end
 
 CSV.write(string(trees, "summarytable_withWrongClades_HyDe.csv"), total_false)
 
@@ -73,60 +79,63 @@ CSV.write(string(trees, "summarytable_withWrongClades_HyDe.csv"), total_false)
 
 total_false = DataFrame(gene_trees = Float64[],  trial_num = Float64[], seq_length = Float64[], 
                         HyDe_fp = Float64[], HyDe_fn = Float64[], HyDe_tp = Float64[], HyDe_tn = Float64[], HyDe_wrongClade = Float64[])
-
 for seq_length in seq_lengths
-    for num_trees in num_gene_trees
-        for file_number in 1:30
-            file = DataFrame(CSV.File(string("seq", seq_length, "/", trees, "_n", num_trees, "/", trees,"_", num_trees, "_", file_number, "_", seq_length, "_HyDe.csv")))
-            bonLevel = 0.05/size(file,1);
-            
-            insertcols!(file, size(file, 2) + 1, :FPB => 0);
-            insertcols!(file, size(file, 2) + 1, :FNB => 0);
-            insertcols!(file, size(file, 2) + 1, :TPB => 0);
-            insertcols!(file, size(file, 2) + 1, :TNB => 0);
-            insertcols!(file, size(file, 2) + 1, :WCB => 0);
+    for net_names in network_names
+        net_num = net_names[1:3]
+        for num_trees in num_gene_trees
+            for file_number in 1:30
+                filename = string(net_names,"/HyDe_Dstat/",net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat.csv")
 
-            for row in 1:size(file, 1)
+                file = DataFrame(CSV.File(filename))
+                bonLevel = 0.05/size(file,1);
+
+                insertcols!(file, size(file, 2) + 1, :FPB => 0);
+                insertcols!(file, size(file, 2) + 1, :FNB => 0);
+                insertcols!(file, size(file, 2) + 1, :TPB => 0);
+                insertcols!(file, size(file, 2) + 1, :TNB => 0);
+                insertcols!(file, size(file, 2) + 1, :WCB => 0);
+
+                for row in 1:size(file, 1)
                 
-                pValHybrid = file[row, :Pvalue];
+                    pValHybrid = file[row, :Pvalue];
 
-                HybPredicted = file[row, :HyDeHybrid]
-                HybTriple = file[row, :HybridTripletExpected]
-                HybIDCorrect = file[row, :HybridCorrectID]
+                    HybPredicted = file[row, :HyDeHybrid]
+                    HybTriple = file[row, :HybridTripletExpected]
+                    HybIDCorrect = file[row, :HybridCorrectID]
 
-                if pValHybrid .<= bonLevel
-                    if HybTriple .== 1
-                        if HybIDCorrect .== 1
-                            file[row, :TPB] = 1
+                    if pValHybrid .<= bonLevel
+                        if HybTriple .== 1
+                            if HybIDCorrect .== 1
+                                file[row, :TPB] = 1
+                            else
+                                file[row, :WCB] = 1 #wrong clade is also a "false positive" type result
+                            end
                         else
-                            file[row, :WCB] = 1 #wrong clade is also a "false positive" type result
+                            file[row, :FPB] = 1
                         end
-                    else
-                        file[row, :FPB] = 1
-                    end
-                elseif HybPredicted .== 0
-                    if HybTriple .== 1
-                        if HybIDCorrect .== 2
-                            file[row, :FNB] = 1
+                    elseif HybPredicted .== 0
+                        if HybTriple .== 1
+                            if HybIDCorrect .== 2
+                                file[row, :FNB] = 1
+                            else
+                                file[row, :TNB] = 1
+                            end
                         else
                             file[row, :TNB] = 1
                         end
-                    else
-                        file[row, :TNB] = 1
                     end
                 end
-            end
 
-            HyDe_FP = sum(file[!, :FPB])
-            HyDe_FN = sum(file[!, :FNB])
-            HyDe_TP = sum(file[!, :TPB])
-            HyDe_TN = sum(file[!, :TNB])
-            HyDe_WC = sum(file[!, :WCB])
+                HyDe_FP = sum(file[!, :FPB])
+                HyDe_FN = sum(file[!, :FNB])
+                HyDe_TP = sum(file[!, :TPB])
+                HyDe_TN = sum(file[!, :TNB])
+                HyDe_WC = sum(file[!, :WCB])
 
-            global total_false
-            push!(total_false, [num_trees, file_number, seq_length, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC])
-        end 
+                global total_false
+                push!(total_false, [num_trees, file_number, seq_length, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC])
+            end 
+        end
     end
-end
 
 CSV.write(string(trees, "summarytable_withWrongClades_BonferroniCorrected_HyDe.csv"), total_false)
