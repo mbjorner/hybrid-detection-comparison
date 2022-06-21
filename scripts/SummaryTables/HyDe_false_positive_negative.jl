8a57d108824155519a7f4bbc03009f8c07c52ceb
Marianne Bjørner <mbbjorner@gmail.com>
2022-06-21 16:17:29 -0500

    add ticr individual p values for each quartet

--- scripts/SummaryTables/HyDe_false_positive_negative.jl
+++ scripts/SummaryTables/HyDe_false_positive_negative.jl
@@ -1,36 +1,59 @@
 # Marianne Bjorner
 # plotting false positive and false negative rates
 
-using QuartetNetworkGoodnessFit, DataFrames, CSV, PhyloNetworks
+using DataFrames, CSV, PhyloNetworks
 
 cd("/Users/bjorner/GitHub/phylo-microbes/output/n6out/")
 cd("/Users/bjorner/GitHub/phylo-microbes/output/20220406output/")
+cd("/Users/bjorner/GitHub/phylo-microbes/output/long_mix_out/")
 
-total_false = DataFrame(network_name = String[], gene_trees = Float64[],  trial_num = Float64[], seq_length = Float64[], 
+cd("/Users/bjorner/GitHub/copyn15/")
+cd("/Users/bjorner/github/phylo-microbes/data/HyDe_four_taxon/long_branches")
+
+cd("/Users/bjorner/GitHub/copyn15/")
+
+total_false = DataFrame(network_name = String[], gene_trees = Float64[],  trial_num = Float64[],
                         HyDe_fp = Float64[], HyDe_fn = Float64[], HyDe_tp = Float64[], HyDe_tn = Float64[], HyDe_wrongClade = Float64[],
                         Dstat_fp = Float64[], Dstat_fn = Float64[], Dstat_tp = Float64[], Dstat_tn = Float64[], Dstat_wc = Float64[])
 
-network_names = ["n6", "n10", "n15"] # "n10","n10orange","n10red","n15","n15blue","n15orange","n15red"]
-prefix = "20220406output"
-suffix = string(ARGS[1])
+#total_false = DataFrame(network_name = String[], gene_trees = Float64[],  trial_num = Float64[],
+#                        HyDe_fp = Float64[], HyDe_fn = Float64[], HyDe_tp = Float64[], HyDe_tn = Float64[], HyDe_wrongClade = Float64[])
+                       
 
+network_names = ["n10", "n15"] # "n10","n10orange","n10red","n15","n15blue","n15orange","n15red"]
+network_names = ["long_mix0", "long_mix01", "long_mix02", "long_mix03", "long_mix04", "long_mix05"]
+
+network_names = ["n10","n10red","n10orange", "n15", "n15red", "n15orange", "n15blue"]
+prefix = string(ARGS[1])
+suffix = "n10n15bonbon"
+
+#=
 if suffix == "_lenpertree"
     seq_lengths = [50,100,150,200,500,1000,2000,5000,10000,50000,100000]
 elseif suffix == "_totlen"
     seq_lengths = [15000,30000,60000,105000,255000,510000]
 end
+=#
 
-num_gene_trees = [30,100,300,1000,3000]
+# now know they should be total lengths, simulation of 1bp per tree
+
+num_gene_trees = [3000,10000,50000,100000, 250000, 500000]
+
+num_gene_trees = [50000,100000, 250000, 500000]
 # seq_lengths = [10000,30000,50000,100000,250000,500000]
 # num_gene_trees = [50, 100, 500, 1000]
 for net_names in network_names
     # net_num = net_names[1:2] #[1:3]
     net_num = net_names
-    for seq_length in seq_lengths
+    print("analysing", net_names)
+    #for seq_length in seq_lengths
     for num_trees in num_gene_trees
         for file_number in 1:30
             #filename = string(net_names,"/HyDe_Dstat/",net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat.csv")
-            filename = string(net_names,"/HyDe_Dstat/", net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat", suffix,".csv")
+            #filename = string(net_names,"/HyDe_Dstat/", net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat", suffix,".csv")
+            #filename = string(net_names,num_trees,"-",file_number,"-1.tre_4_HyDe.csv")
+
+            filename = string(net_names, "/", net_names, "-gt", num_trees, "-", file_number, "-1.tre_HyDe_Dstat.csv")
 
             if isfile(filename)
             file = DataFrame(CSV.File(filename))
@@ -58,7 +81,7 @@ for net_names in network_names
                     if HybTriple .== 1
                         if HybIDCorrect .== 1
                             file[row, :TP] = 1
-                        else
+                        else #hybIDcorr2
                             file[row, :WC] = 1 #wrong clade is also a "false positive" type result
                         end
                     else
@@ -80,11 +103,7 @@ for net_names in network_names
 
                 if D_stat_p .<= 0.05
                     if HybTriple .== 1
-                        if HybIDCorrect .== 1
-                            file[row, :TP_Dstat] = 1
-                        else
-                            file[row, :WC_Dstat] = 1 #wrong clade is also a "false positive" type result
-                        end
+                        file[row, :TP_Dstat] = 1
                     else
                         file[row, :FP_Dstat] = 1
                     end
@@ -115,33 +134,37 @@ for net_names in network_names
             Dstat_WC = sum(file[!, :WC_Dstat])
 
             global total_false
-            push!(total_false, [net_names, num_trees, file_number, seq_length, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC, 
+            push!(total_false, [net_names, num_trees, file_number, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC, 
                                 Dstat_FP, Dstat_FN, Dstat_TP, Dstat_TN, Dstat_WC])
+           # push!(total_false,[net_names, num_trees, file_number, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC])
             end
         end 
     end
 end
-end
+#end
 
 
 CSV.write(string(prefix, "_HyDe_DStat_p05",suffix,".csv"), total_false)
+CSV.write(string(prefix, "_HyDe_p05",suffix,".csv"), total_false)
 
 
 # Now a bonferroni corrected version: where level is determined by 0.05/nrows (number of triplet tests)
 
 
 
-total_false_bf = DataFrame(network_name = String[], gene_trees = Float64[],  trial_num = Float64[], seq_length = Float64[], 
+total_false_bf = DataFrame(network_name = String[], gene_trees = Float64[],  trial_num = Float64[],
                         HyDe_fp = Float64[], HyDe_fn = Float64[], HyDe_tp = Float64[], HyDe_tn = Float64[], HyDe_wrongClade = Float64[],
                         Dstat_fp = Float64[], Dstat_fn = Float64[], Dstat_tp = Float64[], Dstat_tn = Float64[], Dstat_wc = Float64[])
-for seq_length in seq_lengths
+
     for net_names in network_names
         #net_num = net_names[1:3]
         net_num = net_names
         for num_trees in num_gene_trees
             for file_number in 1:30
-                filename = string(net_names,"/HyDe_Dstat/", net_names,".net_",net_num,"_", num_trees, "_", file_number, "_",seq_length, "_HyDe_Dstat", suffix,".csv")
-                
+
+                filename = string(net_names, "-gt", num_trees, "-", file_number, "-1.tre_HyDe_Dstat.csv")
+                filename = string(net_names, "/", net_names, "-gt", num_trees, "-", file_number, "-1.tre_HyDe_Dstat.csv")
+
                 if isfile(filename)
                 
                 file = DataFrame(CSV.File(filename))
@@ -197,11 +220,7 @@ for seq_length in seq_lengths
 
                     if D_stat_p .<= bonLevel
                         if HybTriple .== 1
-                            if HybIDCorrect .== 1
-                                file[row, :TPB_Dstat] = 1
-                            else
-                                file[row, :WCB_Dstat] = 1 #wrong clade is also a "false positive" type result
-                            end
+                            file[row, :TPB_Dstat] = 1
                         else
                             file[row, :FPB_Dstat] = 1
                         end
@@ -232,12 +251,12 @@ for seq_length in seq_lengths
                 Dstat_WC = sum(file[!, :WCB_Dstat])
 
                 global total_false_bf
-                push!(total_false_bf, [net_names, num_trees, file_number, seq_length, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC, 
+                push!(total_false_bf, [net_names, num_trees, file_number, HyDe_FP, HyDe_FN, HyDe_TP, HyDe_TN, HyDe_WC, 
                                     Dstat_FP, Dstat_FN, Dstat_TP, Dstat_TN, Dstat_WC])
                 end
             end 
         end
     end
-end
+
 
 CSV.write(string(prefix, "_HyDe_DStat_pbonferroni",suffix,".csv"), total_false_bf)
--- scripts/SummaryTables/ticr_msc_summary.jl
+++ scripts/SummaryTables/ticr_msc_summary.jl
@@ -12,7 +12,12 @@ using QuartetNetworkGoodnessFit, DataFrames, CSV, PhyloNetworks
 # gene_trees
 # trial_number
 # tice_pVal    is overall p value from TICR
-ticrPOverall = DataFrame(network = String[], gene_trees = Float64[],  trial_num = Float64[], ticr_pVal = Float64[])
+
+ticrPOverall_alphaBonferroni = DataFrame(network = String[], gene_trees = Float64[],  trial_num = Float64[], ticr_pVal = Float64[], 
+                        FP=Float64[], FN=Float64[], TP=Float64[], TN=Float64[]) 
+ticrPOverall_alpha05 = DataFrame(network = String[], gene_trees = Float64[],  trial_num = Float64[], ticr_pVal = Float64[], 
+                        FP=Float64[], FN=Float64[], TP=Float64[], TN=Float64[]) 
+                        
 MSC_results_alpha05 = DataFrame(network = String[], gene_trees = Float64[],  trial_num = Float64[], 
                         FP=Float64[], FN=Float64[], TP=Float64[], TN=Float64[])
 MSC_results_alphaBonferroni = DataFrame(network = String[], gene_trees = Float64[],  trial_num = Float64[], 
@@ -20,45 +25,47 @@ MSC_results_alphaBonferroni = DataFrame(network = String[], gene_trees = Float64
 
 # Step 1: navigate to overall directory containing results
 cd("/Users/bjorner/GitHub/phylo-microbes/output/2022FEB15_output_alln10_n15/")
-
-cd("/Users/bjorner/GitHub/phylo-microbes/output/20220406output/")
-
+cd("/Users/bjorner/GitHub/phylo-microbes/output/20220511output/")
 
 network_names = ["n10","n10orange","n10red","n15","n15blue","n15orange","n15red"]
 num_gene_trees = [30,100,300,1000,3000]
 
-network_names = ["n6","n10","n15"]
-num_gene_trees = [30,100,300,1000,3000]
+#network_names = ["n6","n10","n15"]
+#num_gene_trees = [30,100,300,1000,3000]
+#network_names = ["n10","n10red","n10orange"]
+#num_gene_trees = [30,100,300,1000,3000,10000,50000,100000,250000,500000]
+
+#  /Users/bjorner/GitHub/phylo-microbes/output/20220511output/n10-gt300-11-1.tre_TICR_MSC_summary.csv
 
 for net_names in network_names
-
-    #net_num = net_names[1:3]
-    net_num = net_names
-
+    # net_num = net_names
     for num_trees in num_gene_trees
         for file_number in 1:30
-            filename = string(net_names,"/",net_names,".net_",net_num,"_", num_trees, "_", file_number, "_TICR_MSC_summary.csv")
-            
+            filename = string(net_names, "-gt", num_trees, "-", file_number, "-1.tre_TICR_MSC_summary.csv")
+            if isfile(filename)
                 file = DataFrame(CSV.File(filename))
-            
-                pVal = file[1,:overallPval]
 
-                global ticrPOverall
-                push!(ticrPOverall, [net_names, num_trees, file_number, pVal])
+                ticr_pVal = file[1, :overallPval]
 
                 # identify number of false positive, negatives, etc. within MSC values of table, depending on p-value (05, or bonferroni corrected)
-              
 
-                insertcols!(file, size(file, 2) + 1, :MSC_FP => 0);
-                insertcols!(file, size(file, 2) + 1, :MSC_FN => 0);
-                insertcols!(file, size(file, 2) + 1, :MSC_TP => 0);
-                insertcols!(file, size(file, 2) + 1, :MSC_TN => 0);
-                insertcols!(file, size(file, 2) + 1, :HYB_EX => convert.(Int64, file[!, :hybridExpected]));
+
+                insertcols!(file, size(file, 2) + 1, :MSC_FP => 0)
+                insertcols!(file, size(file, 2) + 1, :MSC_FN => 0)
+                insertcols!(file, size(file, 2) + 1, :MSC_TP => 0)
+                insertcols!(file, size(file, 2) + 1, :MSC_TN => 0)
+
+                insertcols!(file, size(file, 2) + 1, :TICR_FP => 0)
+                insertcols!(file, size(file, 2) + 1, :TICR_FN => 0)
+                insertcols!(file, size(file, 2) + 1, :TICR_TP => 0)
+                insertcols!(file, size(file, 2) + 1, :TICR_TN => 0)
+
+                insertcols!(file, size(file, 2) + 1, :HYB_EX => convert.(Int64, file[!, :hybridExpected]))
 
                 for row in 1:size(file, 1)
                     hyb = file[row, :HYB_EX]
-                    alpha = 0.05/size(file,1)
-                    if file[row, :MSC_pVal] .< alpha
+                    alpha = 0.05 / size(file, 1)
+                    if file[row, :MSC_pVal] .<= alpha
                         if hyb == 0
                             file[row, :MSC_FP] = 1
                         else # hyb == 1
@@ -68,20 +75,42 @@ for net_names in network_names
                         if hyb == 1
                             file[row, :MSC_FN] = 1
                         else # hyb == 0
-                            file[row, :MSC_FP] = 1
+                            file[row, :MSC_TN] = 1
                         end
                     end
+
+                    if file[row, :pValTicr] .<= alpha
+                        if hyb == 0
+                            file[row, :TICR_FP] = 1
+                        else # hyb == 1
+                            file[row, :TICR_TP] = 1
+                        end
+                    elseif file[row, :pValTicr] .> alpha
+                        if hyb == 1
+                            file[row, :TICR_FN] = 1
+                        else # hyb == 0
+                            file[row, :TICR_TN] = 1
+                        end
+                    end
+
                 end
+
 
                 MSC_FP = sum(file[!, :MSC_FP])
                 MSC_FN = sum(file[!, :MSC_FN])
-                TRUE_POS = sum(file[!, :MSC_TP])
+                TRUE_POS = sum(file[!, :MSC_TP]) 
                 TRUE_NEG = sum(file[!, :MSC_TN])
-                # TRUE_POS = sum(file[!, :HYB_EX])
-                # TRUE_NEG = size(file, 1) - TRUE_POS
-                
+
                 global MSC_results_alphaBonferroni
                 push!(MSC_results_alphaBonferroni, [net_names, num_trees, file_number, MSC_FP, MSC_FN, TRUE_POS, TRUE_NEG])
+
+                TICR_FP_SUM = sum(file[!, :TICR_FP])
+                TICR_FN_SUM = sum(file[!, :TICR_FN])
+                TICR_TP_SUM = sum(file[!, :TICR_TP])
+                TICR_TN_SUM = sum(file[!, :TICR_TN])
+
+                global ticrPOverall_alphaBonferroni
+                push!(ticrPOverall_alphaBonferroni, [net_names, num_trees, file_number, ticr_pVal, TICR_FP_SUM, TICR_FN_SUM, TICR_TP_SUM, TICR_FN_SUM])
 
                 for row in 1:size(file, 1)
                     hyb = file[row, :HYB_EX]
@@ -90,10 +119,16 @@ for net_names in network_names
                     file[row, :MSC_FN] = 0
                     file[row, :MSC_TP] = 0
                     file[row, :MSC_TN] = 0
-                    if file[row, :MSC_pVal] .< alpha
+
+                    file[row, :TICR_FP] = 0
+                    file[row, :TICR_FN] = 0
+                    file[row, :TICR_TP] = 0
+                    file[row, :TICR_TN] = 0
+
+                    if file[row, :MSC_pVal] .<= alpha
                         if hyb == 0
                             file[row, :MSC_FP] = 1
-                        else 
+                        else
                             file[row, :MSC_TP] = 1
                         end
                     elseif file[row, :MSC_pVal] .> alpha
@@ -103,27 +138,52 @@ for net_names in network_names
                             file[row, :MSC_TN] = 1
                         end
                     end
+
+                    if file[row, :pValTicr] .<= alpha
+                        if hyb == 0
+                            file[row, :TICR_FP] = 1
+                        else # hyb == 1
+                            file[row, :TICR_TP] = 1
+                        end
+                    elseif file[row, :pValTicr] .> alpha
+                        if hyb == 1
+                            file[row, :TICR_FN] = 1
+                        else # hyb == 0
+                            file[row, :TICR_TN] = 1
+                        end
+                    end
+
                 end
 
                 MSC_FP = sum(file[!, :MSC_FP])
                 MSC_FN = sum(file[!, :MSC_FN])
-                TRUE_POS = sum(file[!, :HYB_EX])
-                TRUE_NEG = size(file, 1) - TRUE_POS
+                TRUE_POS = sum(file[!, :MSC_TP]) 
+                TRUE_NEG = sum(file[!, :MSC_TN])
 
                 global MSC_results_alpha05
                 push!(MSC_results_alpha05, [net_names, num_trees, file_number, MSC_FP, MSC_FN, TRUE_POS, TRUE_NEG])
 
-            
+
+                TICR_FP_SUM = sum(file[!, :TICR_FP])
+                TICR_FN_SUM = sum(file[!, :TICR_FN])
+                TICR_TP_SUM = sum(file[!, :TICR_TP])
+                TICR_TN_SUM = sum(file[!, :TICR_TN])
+
+                global ticrPOverall_alpha05
+                push!(ticrPOverall_alpha05, [net_names, num_trees, file_number, ticr_pVal, TICR_FP_SUM, TICR_FN_SUM, TICR_TP_SUM, TICR_FN_SUM])
+
             end
         end
     end
 end 
 
 prefix = ARGS[1]
-ticrout = "_ticr_results.csv"
+ticrout05 = "_ticr05_results.csv"
+ticrPOverall_alphaBonferroni = "_ticrbonferroni_results.csv"
 msc05 = "_msc05_results.csv"
 mscbonferroni = "_mscbonferroni_results.csv"
 
 CSV.write(string(prefix,mscbonferroni), MSC_results_alphaBonferroni)
 CSV.write(string(prefix,msc05), MSC_results_alpha05)
-CSV.write(string(prefix,ticrout), ticrPOverall)
+CSV.write(string(prefix,ticroutbonferroni), ticrPOverall_alphaBonferroni)
+CSV.write(string(prefix,ticrout05), ticrPOverall_alpha05)
