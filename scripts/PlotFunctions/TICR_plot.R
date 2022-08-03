dfb4be8901f0e0fca86a66be32bddd7dda805856
Marianne Bjørner <mbbjorner@gmail.com>
2022-08-03 12:41:09 -0500

    plot CPU times of methods

--- scripts/PlotFunctions/TICR_plot.R
+++ scripts/PlotFunctions/TICR_plot.R
@@ -9,6 +9,8 @@ setwd("~/GitHub/phylo-microbes/output/2022FEB15_output_alln10_n15")
 
 inputFile = "2022FEB15_TICR_results_withQMC.csv"
 
+setwd("~/GitHub/phylo-microbes/data/foranal20220517/")
+inputFile = "n10n15_TICR_results_majorTree20220518.csv"
 # read input file as a dataframe for use with ggplot2;
 # ggplot2 must be installed w accompanying packages.
 
@@ -21,7 +23,7 @@ TICR_data = read.csv(inputFile);
 # the number of gene trees; we should do this for each of the trees
 # that we have:
 
-TICR_counts <- TICR_data %>% group_by(network, gene_trees)
+TICR_counts <- TICR_data %>% group_by(net_name, gene_trees)
 
 alpha = 0.05;
 
@@ -32,9 +34,15 @@ summary_TICR <- TICR_counts %>% summarise(
 
 # goal is now to plot the summary_TICR that we have created 
 
-ggplot(data = summary_TICR, mapping = aes(x = network, y = numSignificantAlpha, fill=factor(gene_trees))) +
+ggplot(data = summary_TICR, mapping = aes(x = net_name, y = numSignificantAlpha, fill=factor(gene_trees))) +
   geom_bar(position="dodge",stat="identity") + ylab("number of trials (of 30) with significant results") + 
   xlab("Number of Gene Trees") + ggtitle("Number of tests rejecting true network");
+
+# plot instead the proportion
+ggplot(data = summary_TICR, mapping = aes(x = net_name, y = numSignificantAlpha/30, fill = factor(gene_trees))) +
+  geom_bar(position="dodge",stat="identity") + ylab("Proportion Correctly Rejecting Major Tree") + 
+  xlab("Input Network") + ggtitle("Proportion of TICR tests rejecting Major Tree as True Topology") + labs(fill = "Number of Gene Trees") + theme_classic();
+
 
 ggplot(data = summary_TICR, mapping = aes(x = network, y = numSignificantBonferroni, fill=factor(gene_trees))) +
   geom_bar(position="dodge",stat="identity")
--- scripts/PlotFunctions/plot_timing.R
+++ scripts/PlotFunctions/plot_timing.R
@@ -1,13 +1,18 @@
 # with HyDe, D-Stat (ABBA-BABA), TICR, MSCQuartets
 
 library(gridExtra)
+library(dplyr)
+library(ggplot2)
 
 ## method ## network ## network size ## gene_trees/seq_length ## trial ## time_taken (seconds)
-setwd("~/GitHub/phylo-microbes/output/timing")
-timingresults <- "time_comparison.csv"
+setwd("~/GitHub/phylo-microbes/output/ms_out/output08022022/")
+
+# GitHub/phylo-microbes/output/ms_out/output08022022/time_summary.csv
+
+timingresults <- "time_summary.csv"
 
 time_data <- read.csv(timingresults)
-time_grouped <- time_data %>% group_by(method, network, gene_trees)
+time_grouped <- time_data %>% group_by(method, netsize, gt_number)
 
 # Calculate summary values of time taken for all functions
 
@@ -17,40 +22,37 @@ time_grouped <- time_data %>% group_by(method, network, gene_trees)
 # time taken (standard deviation)
 
 summary_times <- time_grouped %>% summarise(
-  time = mean(time_taken),
-  time_sd = stdev(time_taken)
+  time_in_s = mean(cpu_time),
+  time_sd = sd(cpu_time)
 )
 
-network_names <- c("n10", "n10red", "n10orange", "n15",
-                   "n15red", "n15orange", "n15blue")
-network_sizes <- c(4, 5, 8, 10, 15, 25)
-methods   <- c("HyDe", "D-statistic", "D3", "TICR", "MSCquartets")
+# network_names <- c("n10", "n10red", "n10orange", "n15",
+#                   "n15red", "n15orange", "n15blue")
+# network_sizes <- c(4, 5, 8, 10, 15, 25)
+methods   <- c("HyDe", "TICR", "MSC")
 time_plot <- list()
 
 # Plots are arranged with number of taxa on the x axis,
 # and number of gene trees forming separate lines on the plot,
 # with the y-axis as time as measured in seconds.
-
-for (i in seq_len(methods)) {
-  #### Create HyDe plot
+for (i in 1:length(methods)) {
+  
   subset_object <- subset(summary_times, method %in% c(methods[i]))
-
-  time_plot_curr <- ggplot(subset_object, aes(x = network_size)) +
-  geom_line(aes(y <- time), color = "darkred") +
-  geom_point(aes(y = WHR), color = "darkred") +
-  geom_errorbar(
+  
+  time_plot_curr <- ggplot(data = subset_object, mapping = aes(x = netsize, y = time_in_s, color=factor(gt_number))) +
+  geom_line() +geom_point() + ylab("CPU time (s)") + xlab("Network Size (number of taxa)") + ggtitle("") +  geom_errorbar(
     data = subset_object,
-    aes(gene_trees, time, ymin = time - time_sd, ymax = time + time_sd),
-    colour = 'darkred',
-    width = 0.2) +
-  theme_classic() + theme(axis.title = element_blank()) + ylim(0,1) + title(methods[i])
-
+    aes(netsize, time_in_s, ymin = time_in_s - time_sd, ymax = time_in_s + time_sd),
+    width = 0.2) + ggtitle(methods[i]) + labs(colour = "Number of Gene Trees / Sequence Length") +
+  theme_classic();
+  
   time_plot[[i]] <- time_plot_curr
 }
 
+
 # plots are arranged with a single plot corresponding to the time for each method, 
 
-# methods   <- c("HyDe", "D-statistic", "D3", "TICR", "MSCquartets")
+# methods   <- c("HyDe", "TICR", "MSCquartets")
 
-grid.arrange(time_plot[1], time_plot[2], time_plot[3], time_plot[4], time_plot[5])
+grid.arrange(time_plot[1], time_plot[2], time_plot[3])
 
