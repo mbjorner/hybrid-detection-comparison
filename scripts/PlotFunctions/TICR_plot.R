191bfa8b9966a129389f432141dc15070716425d
Marianne Bjørner <mbbjorner@gmail.com>
2022-09-08 13:25:18 -0500

    update plotting functions

--- scripts/PlotFunctions/TICR_plot.R
+++ scripts/PlotFunctions/TICR_plot.R
@@ -11,6 +11,8 @@ inputFile = "2022FEB15_TICR_results_withQMC.csv"
 
 setwd("~/GitHub/phylo-microbes/data/foranal20220517/")
 inputFile = "n10n15_TICR_results_majorTree20220518.csv"
+
+inputFile = "/Users/bjorner/GitHub/phylo-microbes/scripts/CHTCFunctions/ms_outfiles_paper_ticr.csv"
 # read input file as a dataframe for use with ggplot2;
 # ggplot2 must be installed w accompanying packages.
 
@@ -23,25 +25,51 @@ TICR_data = read.csv(inputFile);
 # the number of gene trees; we should do this for each of the trees
 # that we have:
 
-TICR_counts <- TICR_data %>% group_by(net_name, gene_trees)
+TICR_counts <- TICR_data %>% group_by(network_name, seq_length)
 
 alpha = 0.05;
 
 summary_TICR <- TICR_counts %>% summarise(
-  numSignificantAlpha = sum(ticr_pVal < alpha, na.rm=TRUE),
-  numSignificantBonferroni = sum(ticr_pVal < alpha/30, na.rm=TRUE)
+  numSignificantAlpha = sum(TICR_pval < alpha, na.rm=TRUE),
+  numSignificantBonferroni = sum(TICR_pval < alpha/30, na.rm=TRUE)
 )
 
 # goal is now to plot the summary_TICR that we have created 
 
-ggplot(data = summary_TICR, mapping = aes(x = net_name, y = numSignificantAlpha, fill=factor(gene_trees))) +
+subset
+
+ggplot(data = summary_TICR, mapping = aes(x = network_name, y = numSignificantAlpha, fill=factor(seq_length))) +
   geom_bar(position="dodge",stat="identity") + ylab("number of trials (of 30) with significant results") + 
   xlab("Number of Gene Trees") + ggtitle("Number of tests rejecting true network");
 
 # plot instead the proportion
-ggplot(data = summary_TICR, mapping = aes(x = net_name, y = numSignificantAlpha/30, fill = factor(gene_trees))) +
+net101525 = c("n10h2.net"    ,  
+              "n10red.net"     ,   "n10orange.net"  ,  
+              "n15h3.net" ,  "n15blue.net"     ,       "n15red.net" ,  "n15orange.net" , 
+              "n25h5.net")
+net458 = c("n4h1_0.1.net",     "n4h1_0.2.net"   ,    "n4h1_0.3.net" ,    "n4h1_0.4.net"  ,  "n4h1_0.4.net",
+              "n4h1_0.5.net"    ,  "n4h1_0.net" ,  "n4h1_introg.net", "n5h2_overlap.net", "n8h3.net" )
+
+summary_TICR_101525 = subset(summary_TICR, network_name %in% net101525)
+summary_TICR_458 = subset(summary_TICR, network_name %in% net458)
+library(stringr)
+replace(summary_TICR_101525$network_name, grepl(".net",summary_TICR_101525$network_name), str_replace(summary_TICR_101525$network_name, ".net", "")) 
+#summary_TICR_458<-replace(summary_TICR_101525$network_name, grepl(".net",summary_TICR_101525$network_name), str_replace(summary_TICR_101525$network_name, ".net", "")) 
+
+
+
+ggplot(data = summary_TICR, mapping = aes(x = network_name, y = numSignificantBonferroni/30, fill = factor(seq_length))) +
   geom_bar(position="dodge",stat="identity") + ylab("Proportion Correctly Rejecting Major Tree") + 
   xlab("Input Network") + ggtitle("Proportion of TICR tests rejecting Major Tree as True Topology") + labs(fill = "Number of Gene Trees") + theme_classic();
+
+ggplot(data = summary_TICR_101525, mapping = aes(x = network_name, y = numSignificantBonferroni/30, fill = factor(seq_length))) +
+  geom_bar(position="dodge",stat="identity") + ylab("Proportion Correctly Rejecting Major Tree") + 
+  xlab("Network") + ggtitle("Proportion of TICR tests rejecting Major Tree as True Topology") + labs(fill = "Number of Gene Trees") + theme_classic();
+
+ggplot(data = summary_TICR_458, mapping = aes(x = network_name, y = numSignificantBonferroni/30, fill = factor(seq_length))) +
+  geom_bar(position="dodge",stat="identity") + ylab("Proportion Correctly Rejecting Major Tree") + 
+  xlab("Network") + ggtitle("Proportion of TICR tests rejecting Major Tree as True Topology") + labs(fill = "Number of Gene Trees") + theme_classic();
+
 
 
 ggplot(data = summary_TICR, mapping = aes(x = network, y = numSignificantBonferroni, fill=factor(gene_trees))) +
--- scripts/PlotFunctions/plot_timing.R
+++ scripts/PlotFunctions/plot_timing.R
@@ -10,6 +10,7 @@ setwd("~/GitHub/phylo-microbes/output/ms_out/output08022022/")
 # GitHub/phylo-microbes/output/ms_out/output08022022/time_summary.csv
 
 timingresults <- "time_summary.csv"
+timingresults <- "/Users/bjorner/GitHub/phylo-microbes/output/ms_out/time_summary_ms_paper.csv"
 
 time_data <- read.csv(timingresults)
 time_grouped <- time_data %>% group_by(method, netsize, gt_number)
@@ -24,6 +25,7 @@ time_grouped <- time_data %>% group_by(method, netsize, gt_number)
 summary_times <- time_grouped %>% summarise(
   time_in_s = mean(cpu_time),
   time_sd = sd(cpu_time)
+  
 )
 
 # network_names <- c("n10", "n10red", "n10orange", "n15",
@@ -37,22 +39,54 @@ time_plot <- list()
 # with the y-axis as time as measured in seconds.
 for (i in 1:length(methods)) {
   
-  subset_object <- subset(summary_times, method %in% c(methods[i]))
+  subset_object <- subset(subset_24_and_below, method %in% c(methods[i]))
   
   time_plot_curr <- ggplot(data = subset_object, mapping = aes(x = netsize, y = time_in_s, color=factor(gt_number))) +
   geom_line() +geom_point() + ylab("CPU time (s)") + xlab("Network Size (number of taxa)") + ggtitle("") +  geom_errorbar(
     data = subset_object,
     aes(netsize, time_in_s, ymin = time_in_s - time_sd, ymax = time_in_s + time_sd),
-    width = 0.2) + ggtitle(methods[i]) + labs(colour = "Number of Gene Trees / Sequence Length") +
+    width = 0.2) + ggtitle(methods[i]) + labs(colour = "Number of Gene Trees") +
   theme_classic();
   
   time_plot[[i]] <- time_plot_curr
 }
+#HYDE PLOT
+subset_object <- subset(summary_times, "HyDe" %in% c(methods[i]))
+
+HyDe_plot <- ggplot(data = subset_object, mapping = aes(x = netsize, y = time_in_s, color=factor(gt_number))) +
+  geom_line() +geom_point() + ylab("CPU time (s)") + xlab("Network Size (number of taxa)") + ggtitle("") +  geom_errorbar(
+    data = subset_object,
+    aes(netsize, time_in_s, ymin = time_in_s - time_sd, ymax = time_in_s + time_sd),
+    width = 0.2) + ggtitle("HyDe") + labs(colour = "Sequence Length (bp)") +
+  theme_classic();
+
+# TICR PLOT
+subset_object <- subset(summary_times, "TICR" %in% c(methods[i]))
+
+TICR_plot <- ggplot(data = subset_object, mapping = aes(x = netsize, y = time_in_s, color=factor(gt_number))) +
+  geom_line() +geom_point() + ylab("CPU time (s)") + xlab("Network Size (number of taxa)") + ggtitle("") +  geom_errorbar(
+    data = subset_object,
+    aes(netsize, time_in_s, ymin = time_in_s - time_sd, ymax = time_in_s + time_sd),
+    width = 0.2) + ggtitle("TICR") + labs(colour = "Number of Gene Trees") +
+  theme_classic();
+
+#MSC PLOT
+subset_object <- subset(summary_times, "MSC" %in% c(methods[i]))
+
+MSC_plot <- ggplot(data = subset_object, mapping = aes(x = netsize, y = time_in_s, color=factor(gt_number))) +
+  geom_line() +geom_point() + ylab("CPU time (s)") + xlab("Network Size (number of taxa)") + ggtitle("") +  geom_errorbar(
+    data = subset_object,
+    aes(netsize, time_in_s, ymin = time_in_s - time_sd, ymax = time_in_s + time_sd),
+    width = 0.2) + ggtitle("MSCquartets") + labs(colour = "Number of Gene Trees") +
+  theme_classic();
+
+
 
 
 # plots are arranged with a single plot corresponding to the time for each method, 
 
 # methods   <- c("HyDe", "TICR", "MSCquartets")
 
-grid.arrange(time_plot[1], time_plot[2], time_plot[3])
+grid.arrange(time_plot[[1]], time_plot[[2]], time_plot[[3]], common.legend = TRUE, legend ="right")
 
+grid.arrange(HyDe_plot, TICR_plot, MSC_plot)
